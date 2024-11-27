0c83fd15dbab63e1692c6ff9bea7c3ac8a462e9e
AyoMoses1 <ayomoses111@gmail.com>
2024-11-27 04:48:09 +0100

    update migration files

--- api/src/user/entities/user.entity.ts
+++ api/src/user/entities/user.entity.ts
@@ -28,6 +28,27 @@ export class UserEntity extends BaseEntity {
   })
   passwordHash: string;
 
+  @Column({
+    name: 'email_verified',
+    type: 'boolean',
+    default: false,
+  })
+  emailVerified: boolean;
+
+  @Column({
+    name: 'email_verification_token',
+    type: 'varchar',
+    nullable: true,
+  })
+  emailVerificationToken: string | null;
+
+  @Column({
+    name: 'email_verification_token_expires_at',
+    type: 'timestamp',
+    nullable: true,
+  })
+  emailVerificationTokenExpiresAt: Date | null;
+
   @Column({ nullable: true })
   token: string;
 }
