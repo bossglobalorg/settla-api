4ed5f1eb740789ff2c64de211ebf0a217fcd78c2
AyoMoses1 <ayomoses111@gmail.com>
2025-02-04 11:35:22 +0100

    create a generic partner reference entity

---
+++ api/src/global/enums/partner-reference.enum.ts
@@ -0,0 +1,10 @@
+// src/common/enums/partner-reference.enum.ts
+export enum PartnerEntityType {
+  USER = 'user',
+  BUSINESS = 'business',
+}
+
+export enum PartnerName {
+  GRAPH = 'graph',
+  // Add other partners here
+}
