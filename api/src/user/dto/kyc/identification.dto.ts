46ea003c93e3982dec6825300f2c76ecabd92e54
AyoMoses1 <ayomoses111@gmail.com>
2025-02-06 12:32:57 +0100

    update create identification dto

--- api/src/user/dto/kyc/identification.dto.ts
+++ api/src/user/dto/kyc/identification.dto.ts
@@ -3,25 +3,25 @@ import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 export class IdentificationDto {
   @IsString()
   @IsEnum(['primary', 'secondary'])
-  id_level: string;
+  idLevel: string;
 
   @IsString()
-  @IsEnum(['passport', 'national_id', 'drivers_license', 'voters_card'])
-  id_type: string;
-
-  @IsString()
-  @IsNotEmpty()
-  id_number: string;
+  @IsEnum(['passport', 'national_id', 'drivers_license', 'voters_card', 'nin'])
+  idType: string;
 
   @IsString()
   @IsNotEmpty()
-  bank_id_number: string;
+  idNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
+  bankIdNumber: string;
 
   @IsString()
   @IsEnum(['NG', 'US'])
-  id_country: string;
+  idCountry: string;
 
   @IsString()
   @IsEnum(['basic', 'preliminary'])
-  kyc_level: string;
+  kycLevel: string;
 }
