1a42b22382fadc93b02fe953208f399cc0965a8d
AyoMoses1 <ayomoses111@gmail.com>
2025-03-14 09:41:07 +0100

    fix user kyc issue

--- src/modules/features/user/services/kyc/kyc.service.ts
+++ src/modules/features/user/services/kyc/kyc.service.ts
@@ -1,45 +1,39 @@
+import { Repository } from 'typeorm'
+
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
-import { UserEntity } from '../../entities/user.entity'
-import { GraphService } from '@providers/graph/graph.service'
-import { PersonalInfoDto } from '@features/user/dto/kyc/personal-info.dto'
-import { IdentificationDto } from '@features/user/dto/kyc/identification.dto'
+
 import { BackgroundInfoDto } from '@features/user/dto/kyc/background-info.dto'
 import { DocumentDto } from '@features/user/dto/kyc/document.dto'
+import { IdentificationDto } from '@features/user/dto/kyc/identification.dto'
+import { PersonalInfoDto } from '@features/user/dto/kyc/personal-info.dto'
+import { GraphService } from '@providers/graph/graph.service'
+
+import { UserEntity } from '../../entities/user.entity'
 
 @Injectable()
 export class UserKycService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
-    private readonly graphService: GraphService
+    private readonly graphService: GraphService,
   ) {}
 
-  async savePersonalInfo(
-    userId: string,
-    data: PersonalInfoDto
-  ): Promise<UserEntity> {
+  async savePersonalInfo(userId: string, data: PersonalInfoDto): Promise<UserEntity> {
     const user = await this.findUser(userId)
     Object.assign(user, data)
     user.kyc_step = 'personal_info'
     return await this.userRepository.save(user)
   }
 
-  async saveIdentification(
-    userId: string,
-    data: IdentificationDto
-  ): Promise<UserEntity> {
+  async saveIdentification(userId: string, data: IdentificationDto): Promise<UserEntity> {
     const user = await this.findUser(userId)
     Object.assign(user, data)
     user.kyc_step = 'identification'
     return await this.userRepository.save(user)
   }
 
-  async saveBackgroundInfo(
-    userId: string,
-    data: BackgroundInfoDto
-  ): Promise<UserEntity> {
+  async saveBackgroundInfo(userId: string, data: BackgroundInfoDto): Promise<UserEntity> {
     const user = await this.findUser(userId)
     Object.assign(user, {
       background_information: data,
@@ -48,10 +42,7 @@ export class UserKycService {
     return await this.userRepository.save(user)
   }
 
-  async addDocument(
-    userId: string,
-    document: DocumentDto
-  ): Promise<UserEntity> {
+  async addDocument(userId: string, document: DocumentDto): Promise<UserEntity> {
     const user = await this.findUser(userId)
 
     if (!user.documents) {
@@ -65,8 +56,7 @@ export class UserKycService {
 
     if (this.isKycComplete(updatedUser)) {
       try {
-        const verificationResult =
-          await this.graphService.verifyUserKyc(updatedUser)
+        const verificationResult = await this.graphService.verifyUserKyc(updatedUser)
         if (verificationResult?.kyc_status === 'verified') {
           user.kyc_status = 'completed'
           user.kyc_step = 'completed'
