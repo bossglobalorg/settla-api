ef05d464d6a2cb574656268f0ce736b2f9b7ee17
AyoMoses1 <ayomoses111@gmail.com>
2025-04-18 06:15:00 +0100

    update the graph utils functions

--- src/modules/providers/graph/graph.utils.ts
+++ src/modules/providers/graph/graph.utils.ts
@@ -1,16 +1,39 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { AxiosError } from 'axios';
+import { AxiosError } from 'axios'
+
+import { Injectable, Logger } from '@nestjs/common'
+
+interface UserAddress {
+  line1: string
+  line2?: string | null
+  city: string
+  state: string
+  country: string
+  postalCode: string
+}
 
 @Injectable()
 export class GraphUtils {
-  private readonly logger = new Logger(GraphUtils.name);
+  private readonly logger = new Logger(GraphUtils.name)
 
   handleGraphError(error: AxiosError) {
     if (error.response?.data) {
-      const errorMessage = error.response.data as string;
-      throw new Error(errorMessage);
+      const errorMessage = error.response.data as string
+      throw new Error(errorMessage)
     }
 
-    throw error;
+    throw error
+  }
+
+  formatAddress(address: UserAddress | null | undefined) {
+    if (!address) return {}
+
+    return {
+      line1: address.line1,
+      line2: address.line2,
+      city: address.city,
+      state: address.state,
+      country: address.country,
+      postal_code: address.postalCode, // Rename here
+    }
   }
 }
